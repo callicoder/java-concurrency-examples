771ec3335ec07a7bbaf086aa0bb73afb45d825b4
Rajeev Kumar Singh <rajeevs@flock.co>
2017-07-11 20:54:04 +0530

    Volatile Keyword Example

---
+++ java-concurrency-issues-and-synchronization/src/VolatileKeywordExample.java
@@ -0,0 +1,30 @@
+public class VolatileKeywordExample {
+    private static volatile boolean sayHello = false;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread thread = new Thread(() -> {
+           while(!sayHello) {
+
+           }
+
+           System.out.println("Hello World!");
+
+           while(sayHello) {
+
+           }
+
+           System.out.println("Good Bye!");
+        });
+
+        thread.start();
+
+        Thread.sleep(1000);
+        System.out.println("Say Hello..");
+        sayHello = true;
+
+        Thread.sleep(1000);
+        System.out.println("Say Bye..");
+        sayHello = false;
+    }
+}
