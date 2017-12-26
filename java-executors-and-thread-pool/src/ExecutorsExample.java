2e4598678cde0fc6e8533fc0bf9558dac98fb9ea
Rajeev Kumar Singh <rajeevs@flock.co>
2017-12-26 10:03:42 +0530

    ExecutorService

--- java-executors-and-thread-pool/src/ExecutorsExample.java
+++ java-executors-and-thread-pool/src/ExecutorsExample.java
@@ -1,5 +1,6 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by rajeevkumarsingh on 09/05/17.
@@ -8,23 +9,42 @@ public class ExecutorsExample {
     public static void main(String[] args) {
         System.out.println("Inside : " + Thread.currentThread().getName());
 
-        System.out.println("Creating Executor Service...");
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        System.out.println("Creating Executor Service with a thread pool of Size 2");
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-        System.out.println("Creating a Runnable...");
-        Runnable runnable = () -> {
-            System.out.println("Inside : " + Thread.currentThread().getName());
+        Runnable task1 = () -> {
+            System.out.println("Executing Task1 inside : " + Thread.currentThread().getName());
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException ex) {
+                throw new IllegalStateException(ex);
+            }
         };
 
-        System.out.println("Submit the task specified by the runnable to the executor service.");
-        executorService.submit(runnable);
-
-        System.out.println("Creating another Runnable...");
-        Runnable runnable2 = () -> {
-            System.out.println("Executing second task inside : " + Thread.currentThread().getName());
+        Runnable task2 = () -> {
+            System.out.println("Executing Task2 inside : " + Thread.currentThread().getName());
+            try {
+                TimeUnit.SECONDS.sleep(4);
+            } catch (InterruptedException ex) {
+                throw new IllegalStateException(ex);
+            }
         };
 
-        System.out.println("Submitting second runnable.");
-        executorService.submit(runnable2);
+        Runnable task3 = () -> {
+            System.out.println("Executing Task3 inside : " + Thread.currentThread().getName());
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException ex) {
+                throw new IllegalStateException(ex);
+            }
+        };
+
+
+        System.out.println("Submitting the tasks for execution...");
+        executorService.submit(task1);
+        executorService.submit(task2);
+        executorService.submit(task3);
+
+        executorService.shutdown();
     }
 }
